df28fb25bae244579d97f687180a48c77c397b77
ogaute <oyvind.gautestad@gmail.com>
2017-09-17 18:54:12 +0200

    Add project files.

---
+++ vehicleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vehicleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
